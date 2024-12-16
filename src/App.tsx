f86f0028488ee1b242ce2066e9482f27b2b6110e
zwemarnhtay <zwemarnhtay@gmail.com>
2024-12-17 00:05:26 +0630

    [added] sidebar and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Sidebar } from "./components/Sidebar";
+import { Dashboard } from "./pages/Dashboard";
 
 
 const App: React.FC = () => {
 
   return (
     <Router>
-        <div className="flex h-screen bg-gray-100">
+        <div className="flex w-screen h-screen overflow-hidden">
             <Sidebar/>
+            <main className="flex-1 bg-gray-100 overflow-y-auto">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+              </Routes>
+            </main>
         </div>
     </Router>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
+import { 
+    HomeIcon, 
+    Users as PatientIcon, 
+    User as DoctorIcon, 
+    Calendar as CalendarIcon } from 'lucide-react';
+
 
 export const Sidebar: React.FC = () =>{
+
+    const location = useLocation();
+
+    const menuItems = [
+        { path: '/', icon: HomeIcon, label: 'Dashboard' },
+        { path: '/patients', icon: PatientIcon, label: 'Patients' },
+        { path: '/doctors', icon: DoctorIcon, label: 'Doctors' },
+        { path: '/appointments', icon: CalendarIcon, label: 'Appointments' }
+    ]
     
     return (
         <>
-        <div className='w-64 bg-white shadow-md'>
+        <div className='w-64 bg-slate-800 shadow-md h-screen'>
+            <div className="p-4 text-center border-b">
+                <h1 className="text-2xl font-bold text-teal-300">PSKM Admin</h1>
+            </div>
             <nav className='p-4'>
-                <Link 
-                to={'/'} 
-                className='flex items-center p-3 mb-2 rounded-lg'>
-                    Dashboard
-                </Link>
-                <Link 
-                to={'/doctors'} 
-                className='flex items-center p-3 mb-2 rounded-lg'>
-                    Doctor
-                </Link>
-                <Link 
-                to={'/patients'} 
-                className='flex items-center p-3 mb-2 rounded-lg'>
-                    Patient
-                </Link>
-
+                {menuItems.map(item => (
+                    <Link 
+                        to={item.path} 
+                        className={`
+                        flex items-center p-3 mb-2 rounded-lg transition-colors duration-200
+                        ${location.pathname === item.path 
+                          ? 'bg-slate-600 text-blue-600' 
+                          : 'hover:bg-slate-500 text-gray-700'}
+                        `}>
+                            <item.icon className="mr-3" size={20} />
+                            {item.label}
+                    </Link>
+                ))}
             </nav>
         </div>
         </>
